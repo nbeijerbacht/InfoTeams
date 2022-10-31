77b8c5c2885e6d0fbd323198925aa8a34d87bbb7
Nick Beijerbacht <beijerbachtn@gmail.com>
2022-10-31 15:23:03 +0100

    test(ZenyaFacade): Added endpoint tests to the facade layer.

---
+++ ZenyaFacadeTest/MockData.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZenyaFacadeTest
+{
+    public static class MockData
+    {
+        public static string mockAllForms = @"[
+                                        {
+                                            ""type"": ""full"",
+                                            ""description"": ""Something to test with"",
+                                            ""message_after_sending"": """",
+                                            ""language"": ""nl - NL"",
+                                            ""form_id"": 2216,
+                                            ""title"": ""Miha Test Fontys"",
+                                            ""design"": null
+                                        },
+                                        {
+                                            ""type"": ""full"",
+                                            ""description"": ""Testformulier voor random studenten"",
+                                            ""message_after_sending"": """",
+                                            ""language"": ""nl - NL"",
+                                            ""form_id"": 2220,
+                                            ""title"": ""something random"",
+                                            ""design"": null
+                                        }
+                                        ]";
+        public static string searchResult = @"[{
+                                        ""type"": ""full"",
+                                        ""description"": ""Something to test with"",
+                                        ""message_after_sending"": """",
+                                        ""language"": ""nl - NL"",
+                                        ""form_id"": 2216,
+                                        ""title"": ""Miha Test Fontys"",
+                                        ""design"": null
+                                      }]";
+        public static string mockFormById = @"{
+                                        ""type"": ""full"",
+                                        ""description"": ""Something to test with"",
+                                        ""message_after_sending"": """",
+                                        ""language"": ""nl - NL"",
+                                        ""form_id"": 2216,
+                                        ""title"": ""Miha Test Fontys"",
+                                        ""design"": null
+                                      }";
+    }
+}
