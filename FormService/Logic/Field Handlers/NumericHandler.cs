1a74933da83c306cc286c4756dfcbec90a02b4ed
Nick Beijerbacht <beijerbachtn@gmail.com>
2022-11-07 13:33:03 +0100

    removed conflict.

--- FormService/Logic/Field Handlers/NumericHandler.cs
+++ FormService/Logic/Field Handlers/NumericHandler.cs
@@ -2,18 +2,6 @@
 
 namespace FormService.Logic;
 
-<<<<<<< HEAD
-
-public class NumericHandler : IFieldHandler
-{
-    public bool CanHandle(string type) => type is "numeric" or "list" or "numeric_list" or "subject_tree"; 
-
-    public FieldOutput Handle(FieldInput field) => new (field)
-    {
-        value = int.Parse(field.value.ToString()),
-    };
-}
-=======
 public class NumericHandler : IFieldHandler
 {
     public bool CanHandle(string type) => type is "numeric" or "list" or "numeric_list" or "subject_tree";
@@ -23,4 +11,3 @@ public class NumericHandler : IFieldHandler
         value = int.Parse(field.value.ToString()),
     };
 }
->>>>>>> main
