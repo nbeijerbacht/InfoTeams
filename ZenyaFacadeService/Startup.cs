67bfc816428f6059339be5c24be5272db2535b44
Mihail Bondarenco <bondarenco.mihail@gmail.com>
2023-01-09 14:43:30 +0200

    Make ZENYA_API_TOKEN token configurable

--- ZenyaFacadeService/Startup.cs
+++ ZenyaFacadeService/Startup.cs
@@ -1,4 +1,5 @@
-﻿using ZenyaFacadeService.HttpClient;
+﻿using Microsoft.Extensions.Configuration;
+using ZenyaFacadeService.HttpClient;
 
 namespace ZenyaFacadeService;
 
@@ -18,7 +19,11 @@ public class Startup
         services.AddScoped<IZenyaFormHttpClient, ZenyaFormHttpClient>();
         services.AddScoped<IZenyaLookupHttpClient, ZenyaLookupHttpClient>();
         services.AddControllers();
-        services.AddHttpClient("ZenyaClient", c => c.DefaultRequestHeaders.Add("X-Api-Version", "3"));
+        services.AddHttpClient("ZenyaClient", c =>
+        {
+            c.DefaultRequestHeaders.Add("X-Api-Version", "3");
+            c.DefaultRequestHeaders.Add("Authorization", configRoot.GetValue<string>("ZENYA_API_TOKEN"));
+        });
 
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         services.AddEndpointsApiExplorer();
